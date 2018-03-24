0a682515a95b742bd1e340a90c276143e637978e
Livingston, James <jlivingston@paychex.com>
2018-03-23 22:08:28 -0400

    updates

---
+++ PesterDemo/4_ThirdParty/1_SQLServices.Tests.ps1
@@ -0,0 +1,32 @@
+Describe 'SQL Server Services' {     
+
+    Context 'SQL Server service' {
+
+        It 'Should be running' {
+            (Get-Service 'MSSQL$SQL1').Status | Should -Be 'Running'
+        }
+
+        It 'Should be automatic start' {
+            (Get-WmiObject Win32_Service | Where-Object {$_.Name -eq 'MSSQL$SQL1'}).StartMode | Should -Be 'Automatic'
+        }
+
+        It 'Should be using .\SQLSVC' {
+            (Get-WmiObject Win32_Service | Where-Object {$_.Name -eq 'MSSQL$SQL1'}).StartName | Should -Be '.\SQLSVC'
+        }
+
+    }
+    Context 'SQL Server Agent service' {
+
+        It 'Should be running' {
+            (Get-Service 'SQLAgent$SQL1').Status | Should -Be 'Running'
+        }
+
+        It 'Should be automatic start' {
+            (Get-WmiObject Win32_Service | Where-Object {$_.Name -eq 'SQLAgent$SQL1'}).StartMode | Should -Be 'Automatic'
+        }
+
+        It 'Should be using .\SQLSVC' {
+            (Get-WmiObject Win32_Service | Where-Object {$_.Name -eq 'SQLAgent$SQL1'}).StartName | Should -Be '.\SQLSVC'
+        }
+    }
+}
